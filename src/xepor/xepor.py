aa2517d65f800944587a25678b1e624f84bdf13f
ttimasdf <opensource@rabit.pw>
2022-03-17 11:09:13 +0800

    🐛 fix wrongly forwarded request, add feature: blacklist_domain & respect_proxy_headers

--- src/xepor/xepor.py
+++ src/xepor/xepor.py
@@ -10,7 +10,9 @@ import traceback
 import urllib.parse
 from typing import List, Optional, Tuple, Union
 from parse import Parser
+from mitmproxy.connection import Server
 from mitmproxy.http import HTTPFlow, Response
+from mitmproxy.net.http import url
 
 import functools
 import logging
@@ -27,26 +29,44 @@ class InterceptedAPI:
     META_REQ_PASSTHROUGH = "mockapi-request-passthrough"
     META_RESP_PASSTHROUGH = "mockapi-response-passthrough"
     META_REQ_URLPARSE = "mockapi-request-urlparse"
+    META_REQ_HOST = "mockapi-request-host"
 
     REGEX_HOST_HEADER = re.compile(r"^(?P<host>[^:]+|\[.+\])(?::(?P<port>\d+))?$")
+
+    PROXY_FORWARDED_HEADERS = [
+        "X-Forwarded-For",
+        "X-Forwarded-Host",
+        "X-Forwarded-Port",
+        "X-Forwarded-Proto",
+        "X-Forwarded-Server",
+        "X-Real-Ip",
+    ]
 
     def __init__(self,
             default_host: Optional[str]=None,
             host_mapping: List[Tuple[Union[str, re.Pattern], str]]={},
+            blacklist_domain:List[str]=[],
             request_passthrough: bool=True,
-            response_passthrough=True):
+            response_passthrough: bool=True,
+            respect_proxy_headers: bool=False):
 
         self.default_host = default_host
         self.host_mapping = host_mapping
         self.request_routes: List[Tuple[Optional[str], Parser, callable]] = []
         self.response_routes: List[Tuple[Optional[str], Parser, callable]] = []
+        self.blacklist_domain = blacklist_domain
         self.request_passthrough = request_passthrough
         self.response_passthrough = response_passthrough
+        self.respect_proxy_headers = respect_proxy_headers
 
         self._log = logging.getLogger("MockAPI")
         if os.getenv("MOCKAPI_LOG_DEBUG"):
             self._log.setLevel(logging.DEBUG)
         self._log.info("%s started", self.__class__.__name__)
+
+    # def server_connect(self, data: ServerConnectionHookData):
+    #     self._log.debug("Getting connection: peer=%s sock=%s addr=%s . state=%s",
+    #         data.server.peername, data.server.sockname, data.server.address, data.server)
 
     def request(self, flow: HTTPFlow):
         if InterceptedAPI.META_REQ_URLPARSE in flow.metadata:
@@ -63,7 +83,7 @@ class InterceptedAPI:
         if handler is not None:
             self._log.info("<= [%s] %s", flow.request.method, path)
             handler(flow, *params.fixed, **params.named)
-        elif not self.request_passthrough:
+        elif not self.request_passthrough or self.get_host(flow)[0] in self.blacklist_domain:
             self._log.warning("<= [%s] %s default response", flow.request.method, path)
             flow.response = self.default_response()
         else:
@@ -80,11 +100,11 @@ class InterceptedAPI:
         if flow.metadata.get(InterceptedAPI.META_RESP_PASSTHROUGH) is True:
             self._log.warning("=> [%s] %s skipped because of previous passthrough", flow.response.status_code, path)
             return
-        handler, params = self.find_handler(self.get_host(flow), path, InterceptedAPI.RESPONSE)
+        handler, params = self.find_handler(self.get_host(flow)[0], path, InterceptedAPI.RESPONSE)
         if handler is not None:
             self._log.info("=> [%s] %s", flow.response.status_code, path)
             handler(flow, *params.fixed, **params.named)
-        elif not self.response_passthrough:
+        elif not self.response_passthrough or self.get_host(flow)[0] in self.blacklist_domain:
             self._log.warning("=> [%s] %s default response", flow.response.status_code, path)
             flow.response = self.default_response()
         else:
@@ -123,26 +143,32 @@ class InterceptedAPI:
         return wrapper
 
     def remap_host(self, flow: HTTPFlow, overwrite=True):
-        host = self.get_host(flow)
+        host, port = self.get_host(flow)
         for src, dest in self.host_mapping:
-            if isinstance(src, re.Pattern) and src.match(host):
-                if overwrite:
+            if (isinstance(src, re.Pattern) and src.match(host)) or \
+                (isinstance(src, str) and host == src):
+                if overwrite and (flow.request.host != dest or flow.request.port != port):
+                    if self.respect_proxy_headers:
+                        flow.request.scheme = flow.request.headers["X-Forwarded-Proto"]
+                    flow.server_conn = Server((dest, port))
                     flow.request.host = dest
-                return dest
-            elif isinstance(src, str) and host == src:
-                if overwrite:
-                    flow.request.host = dest
+                    flow.request.port = port
+                self._log.debug("flow: %s, remapping host: %s -> %s, port: %d", flow, host, dest, port)
                 return dest
         return host
 
-    def get_host(self, flow: HTTPFlow) -> str:
-        # Get Destination Host
-        if flow.request.host_header is not None:
-            m = self.REGEX_HOST_HEADER.match(flow.request.host_header)
-            if m:
-                return m.group("host").strip("[]")
-        else:
-            return flow.request.pretty_host
+    def get_host(self, flow: HTTPFlow) -> Tuple[str, int]:
+        if InterceptedAPI.META_REQ_HOST not in flow.metadata:
+            if self.respect_proxy_headers:
+                # all(h in flow.request.headers for h in ["X-Forwarded-Host", "X-Forwarded-Port"])
+                host = flow.request.headers["X-Forwarded-Host"]
+                port = int(flow.request.headers["X-Forwarded-Port"])
+            else:
+                # Get Destination Host
+                host, port = url.parse_authority(flow.request.pretty_host, check=False)
+                port = port or url.default_port(flow.request.scheme) or 80
+            flow.metadata[InterceptedAPI.META_REQ_HOST] = (host, port)
+        return flow.metadata[InterceptedAPI.META_REQ_HOST]
 
     def default_response(self):
         return Response.make(404, "Not Found", {"X-Intercepted-By": "mitmproxy"})
@@ -159,7 +185,7 @@ class InterceptedAPI:
             raise ValueError(f"Invalid route type: {reqtype}")
 
         for h, parser, handler in routes:
-            if h is not None and h != host:
+            if h != host:
                 continue
             parse_result = parser.parse(path)
             self._log.debug("Parse %s => %s", path, parse_result)
